948c9c09c684df09dadca6ff524505f5a9e47dc3
LennyDennis <lennydennis@gmail.com>
2020-09-15 13:51:14 +0300

    - Add link to navigate from registered to login screen if registered in fragment_registration.xml - Create the register new user method in the RegistrationFragment.java

--- app/src/main/java/com/lennydennis/leeconsulting/ui/RegistrationFragment.java
+++ app/src/main/java/com/lennydennis/leeconsulting/ui/RegistrationFragment.java
@@ -2,6 +2,7 @@ package com.lennydennis.leeconsulting.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -13,8 +14,16 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.lennydennis.leeconsulting.R;
 import com.lennydennis.leeconsulting.databinding.FragmentRegistrationBinding;
+import com.lennydennis.leeconsulting.util.FragmentUtility;
+
+import java.util.Objects;
 
 public class RegistrationFragment extends Fragment {
 
@@ -22,27 +31,40 @@ public class RegistrationFragment extends Fragment {
     private static final String DOMAIN_NAME = "gmail.com";
 
     private FragmentRegistrationBinding mFragmentRegistrationBinding;
-    private String registrationEmail, registrationPassword, confirmRegistrationPassword;
     private ProgressBar registrationProgressBar;
     private Button registerButton;
+    private String mRegistrationEmail;
+    private String mRegistrationPassword;
+    private String mConfirmRegistrationPassword;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mFragmentRegistrationBinding = FragmentRegistrationBinding.inflate(inflater);
-        registrationEmail = mFragmentRegistrationBinding.etRegistrationEmail.getText().toString();
-        registrationPassword = mFragmentRegistrationBinding.etRegistrationPassword.getText().toString();
-        confirmRegistrationPassword = mFragmentRegistrationBinding.etConfirmPassword.getText().toString();
         registrationProgressBar = mFragmentRegistrationBinding.registrationProgressBar;
         registerButton = mFragmentRegistrationBinding.registerButton;
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mFragmentRegistrationBinding.loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginFragment loginFragment = new LoginFragment();
+                FragmentUtility.replaceFragment(getActivity(),loginFragment,R.id.fragment_host,true);
+            }
+        });
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!registrationEmail.isEmpty() && registrationPassword.isEmpty() && confirmRegistrationPassword.isEmpty()){
-                    if(isValidDomain(registrationEmail)){
-                        if(doStringsMatch(registrationPassword,confirmRegistrationPassword)){
-
+                mRegistrationEmail = mFragmentRegistrationBinding.etRegistrationEmail.getText().toString();
+                mRegistrationPassword = mFragmentRegistrationBinding.etRegistrationPassword.getText().toString();
+                mConfirmRegistrationPassword = mFragmentRegistrationBinding.etConfirmPassword.getText().toString();
+                if(!(mRegistrationEmail.isEmpty() && mRegistrationPassword.isEmpty() && mConfirmRegistrationPassword.isEmpty())){
+                    if(isValidDomain(mRegistrationEmail)){
+                        if(doStringsMatch(mRegistrationPassword, mConfirmRegistrationPassword)){
+                            registerNewUser(mRegistrationEmail,mRegistrationPassword);
                         }else{
                             Toast.makeText(getActivity(), "Passwords do not Match", Toast.LENGTH_SHORT).show();
                         }
@@ -60,6 +82,32 @@ public class RegistrationFragment extends Fragment {
         return mFragmentRegistrationBinding.getRoot();
     }
 
+    private void registerNewUser(String email, String password){
+        showDialog();
+        mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                            Log.d(TAG, "onComplete: "+user);
+                            Toast.makeText(getActivity(), "User successfully registered", Toast.LENGTH_SHORT).show();
+
+                            mFirebaseAuth.signOut();
+                            LoginFragment loginFragment = new LoginFragment();
+                            FragmentUtility.replaceFragment(getActivity(),loginFragment,R.id.fragment_host,true);
+
+                        }else {
+                            Toast.makeText(getActivity(), "registration:failure", Toast.LENGTH_SHORT).show();
+                        }
+                        hideDialog();
+                    }
+                }
+        );
+
+    }
+
     private boolean isValidDomain(String email){
         Log.d(TAG, "isValidDomain: verifying email has correct domain: " + email);
         String domain = email.substring(email.indexOf("@") + 1).toLowerCase();
@@ -68,7 +116,6 @@ public class RegistrationFragment extends Fragment {
     }
 
     private void redirectLoginScreen(){
-
     }
 
     private boolean doStringsMatch(String s1, String s2){
