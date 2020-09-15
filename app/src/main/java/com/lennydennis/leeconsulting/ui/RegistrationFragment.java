442b912bc32aaa5d1977301f5fdcebc29c13abac
LennyDennis <lennydennis@gmail.com>
2020-09-15 11:13:37 +0300

    - Add data binding to the register fragement - Include methods to validate whether fields are empty, domain is good and passwords match

--- app/src/main/java/com/lennydennis/leeconsulting/ui/RegistrationFragment.java
+++ app/src/main/java/com/lennydennis/leeconsulting/ui/RegistrationFragment.java
@@ -4,18 +4,93 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.lennydennis.leeconsulting.R;
+import com.lennydennis.leeconsulting.databinding.FragmentRegistrationBinding;
 
 public class RegistrationFragment extends Fragment {
+
+    private static final String TAG = "RegistrationFragment";
+    private static final String DOMAIN_NAME = "gmail.com";
+
+    private FragmentRegistrationBinding mFragmentRegistrationBinding;
+    private String registrationEmail, registrationPassword, confirmRegistrationPassword;
+    private ProgressBar registrationProgressBar;
+    private Button registerButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false);
+        mFragmentRegistrationBinding = FragmentRegistrationBinding.inflate(inflater);
+        registrationEmail = mFragmentRegistrationBinding.etRegistrationEmail.getText().toString();
+        registrationPassword = mFragmentRegistrationBinding.etRegistrationPassword.getText().toString();
+        confirmRegistrationPassword = mFragmentRegistrationBinding.etConfirmPassword.getText().toString();
+        registrationProgressBar = mFragmentRegistrationBinding.registrationProgressBar;
+        registerButton = mFragmentRegistrationBinding.registerButton;
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isEmpty(registrationEmail) && isEmpty(registrationPassword) && isEmpty(confirmRegistrationPassword)){
+                    if(isValidDomain(registrationEmail)){
+                        if(doStringsMatch(registrationPassword,confirmRegistrationPassword)){
+
+                        }else{
+                            Toast.makeText(getActivity(), "Passwords do not Match", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(getActivity(), "Please Register with Company Email", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(getActivity(), "You must fill out all the fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        hideSoftKeyboard();
+
+        return mFragmentRegistrationBinding.getRoot();
+    }
+
+    private boolean isValidDomain(String email){
+        Log.d(TAG, "isValidDomain: verifying email has correct domain: " + email);
+        String domain = email.substring(email.indexOf("@") + 1).toLowerCase();
+        Log.d(TAG, "isValidDomain: users domain: " + domain);
+        return domain.equals(DOMAIN_NAME);
+    }
+
+    private void redirectLoginScreen(){
+
+    }
+
+    private boolean doStringsMatch(String s1, String s2){
+        return s1.equals(s2);
+    }
+
+    private boolean isEmpty(String string){
+        return string.equals("");
+    }
+
+    private void showDialog(){
+        registrationProgressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideDialog(){
+        if(registrationProgressBar.getVisibility() == View.VISIBLE){
+            registrationProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void hideSoftKeyboard(){
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
